b6f1da4168e74f7eeb2db436a9f716be60457206
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-07-06 10:21:07 +0700

    feat(AppDbContext): add Users DbSet property

--- Database/AppDbContext.cs
+++ Database/AppDbContext.cs
@@ -1,15 +1,22 @@
+using Choi.WebApi.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Choi.WebApi.Database;
 
 public sealed class AppDbContext : DbContext
 {
+    public DbSet<User> Users => Set<User>();
+
     public AppDbContext() : base() { }
     public AppDbContext(DbContextOptions options) : base(options) { }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        base.OnConfiguring(optionsBuilder);
+        if (!optionsBuilder.IsConfigured)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING") ?? throw new NullReferenceException("Missing CONNECTION_STRING environment variable");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
