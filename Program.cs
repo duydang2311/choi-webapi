7d2bef27e20f8bdd13e6b74db01ca918e6cde196
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-07-06 10:21:59 +0700

    refactor: add swagger doc

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +8,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services
     .AddEndpointsApiExplorer()
-    .AddSwaggerGen()
+    .AddSwaggerGen(opt =>
+    {
+        opt.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "Choi. API",
+            Description = "An ASP.NET Core Web API for Choi. app",
+        });
+    })
     .AddDatabase();
 
 var app = builder.Build();
